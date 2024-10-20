d4f91f2e7786bdc7979f94f4305af6fe43b94a67
truongthanhnam <truogthanhnam@gmail.com>
2024-10-20 14:44:58 +0700

    [Front end] define page get list of save job and viewed job in client/user, please create 2 folder: viewed-job, saved-job

--- services/jobService.ts
+++ services/jobService.ts
@@ -1,6 +1,26 @@
-export default api; import api, { fetchData } from './api';
+import api from "./api";
 import endpoint from "@/constants/apiEndpoint";
-
+import { IJobPostCard } from "@/interfaces/IJobPostCard";
+export default class JobService {
+static async getJob(){
+    try {
+      const response = await api.get(endpoint.job.base);
+      return response.data
+    } catch (err) {
+      const error=err as Error;
+      throw new Error(error.message);
+    } 
+  }
+  static async getNewJob(){
+    try {
+      const response = await api.get(endpoint.job.newJobs);
+      return response.data
+    } catch (err) {
+      const error=err as Error;
+      throw new Error(error.message);
+    }
+  }
+}
 export interface Job {
     _id: string;
     jobTitle: string;
