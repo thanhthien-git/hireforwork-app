0f82bc0928f587c15d554c69f86292e0feb84504
truongthanhnam <truogthanhnam@gmail.com>
2024-11-09 16:18:38 +0700

    HFW-74: apply api fetching for job

--- components/job-list/jobManagerTable.tsx
+++ components/job-list/jobManagerTable.tsx
@@ -1,49 +1,17 @@
-import { Button, notification, Popconfirm } from "antd";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { DeleteOutlined, InfoCircleOutlined } from "@ant-design/icons";
-import { useRouter } from "next/router";
 import HeaderSearchComponent from "../header-search/headerSearchComponent";
 import TableCustom from "../tableCustom";
-import JobService from "@/services/jobService";
+import JobService from "@/services/jobService"; 
 
 export default function JobManagerTable() {
   const [jobDocs, setJobDocs] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-
-  const fetchJob = useCallback(async () => {
-    try {
-      setLoading(true);
-      const res = await JobService.get(1, 10);
-      setJobDocs(res.data.docs);
-    } catch (err) {
-      console.error("Error fetching jobs:", err);
-      notification.error({ message: "Error fetching jobs" });
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
-  const handleDelete = useCallback(async (jobId: string) => {
-    try {
-      setLoading(true);
-      await JobService.delete([jobId]); 
-      fetchJob();
-      notification.success({ message: "Job deleted successfully!" });
-    } catch (err) {
-      notification.error({ message: "Failed to delete job" });
-    } finally {
-      setLoading(false);
-    }
-  }, [fetchJob]);
-  
 
   const columns = useMemo(
     () => [
       {
         title: (
           <>
-            <div>Job Title</div>
+            <div>Job title</div>
             <HeaderSearchComponent
               placeholder="Title"
               onChange={(e) => console.log(e.target.value)}
@@ -56,9 +24,9 @@ export default function JobManagerTable() {
       {
         title: (
           <>
-            <div>Company's Name</div>
+            <div>Company's name</div>
             <HeaderSearchComponent
-              placeholder="Company's Name"
+              placeholder="Company's name"
               onChange={(e) => console.log(e.target.value)}
             />
           </>
@@ -84,9 +52,9 @@ export default function JobManagerTable() {
         key: "jobRequirement",
         render: (requirements) => {
           if (Array.isArray(requirements)) {
-            return requirements.join(", ");
+            return requirements.join(", "); 
           }
-          return requirements;
+          return requirements; 
         },
       },
       {
@@ -99,35 +67,22 @@ export default function JobManagerTable() {
         dataIndex: "jobSalaryMax",
         key: "jobSalaryMax",
       },
-      {
-        title: "Action",
-        key: "action",
-        render: (_: any, record: any) => (
-          <>
-            <Popconfirm
-              title="Delete Job"
-              description="Are you sure to delete this job?"
-              onConfirm={() => handleDelete(record._id)}
-              okText="Yes"
-              cancelText="No"
-            >
-              <Button icon={<DeleteOutlined />} />
-            </Popconfirm>
-            <Button
-              type="link"
-              icon={<InfoCircleOutlined />}
-              onClick={() => router.push(`/admin/jobs-manager/${record._id}`)}
-            />
-          </>
-        ),
-      },
     ],
-    [handleDelete, router]
+    []
   );
+
+  const fetchJob = useCallback(async () => {
+    try {
+      const res = await JobService.get(1, 10); 
+      setJobDocs(res.data.docs);
+    } catch (err) {
+      console.error("Error fetching jobs:", err);
+    }
+  }, []);
 
   useEffect(() => {
     fetchJob();
   }, [fetchJob]);
 
-  return <TableCustom columns={columns} dataSource={jobDocs} loading={loading} />;
+  return <TableCustom columns={columns} dataSource={jobDocs} />;
 }
